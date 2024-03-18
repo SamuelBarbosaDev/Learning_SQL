544978e30b92cbdc248836162017b7f599f24656
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-03-18 14:29:58 -0300

    feat: Atualizando informações

---
+++ w3schools/6/update.sql
@@ -0,0 +1,6 @@
+-- Atualizando as informações 
+update cars
+set color = 'red'
+where brand = 'Volvo'
+
+-- Tenha cuidado ao atualizar os registros. Se você omitir o WHERECláusula, Todos os registros serão atualizados!

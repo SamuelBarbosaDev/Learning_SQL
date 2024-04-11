736e5ab3f13a2c6186a71bc34d020266b5092e2e
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-04-11 10:00:55 -0300

    feat: Estudo sobre ´IN´ com ´SELECT´ no ´PostgreSQL´

--- postgres/12_in.sql
+++ postgres/12_in.sql
@@ -4,3 +4,9 @@ SELECT
 FROM customers
 
 where country in ('Germany', 'France', 'UK')
+
+-- Filtrando pela lista de IDs dos clientes
+SELECT
+    *
+FROM customers
+WHERE customer_id in (SELECT customer_id FROM orders);

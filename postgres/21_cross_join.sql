763c3e98f6295e259f82b20cf8901963261ab086
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-05-07 15:16:16 -0300

    feat: Estudo sobre  no

---
+++ postgres/21_cross_join.sql
@@ -0,0 +1,16 @@
+SELECT
+    testproduct_id,
+    product_name,
+    category_id,
+FROM testproduct_id
+
+CROSS JOIN categories
+
+"
+O CROSS JOIN no PostgreSQL combina cada linha de uma tabela com 
+todas as linhas de outra tabela, resultando em uma combinação 
+completa de todas as linhas. Em outras palavras, o CROSS JOIN 
+produz o produto cartesiano das linhas em duas tabelas1234. 
+Diferentemente de outras cláusulas de junção, como LEFT JOIN ou INNER JOIN, 
+a cláusula CROSS JOIN não possui um predicado de junção.
+"

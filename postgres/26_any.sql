bdd291ce461245b86098c96aaf53dc21b9caa9d2
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-05-08 13:32:32 -0300

    feat: Estudo sobre ANY no PostgreSQL

---
+++ postgres/26_any.sql
@@ -0,0 +1,17 @@
+SELECT
+	product_name
+FROM products
+
+WHERE product_id = ANY(
+	SELECT
+		product_id
+	FROM order_details
+	WHERE quantity > 120
+)
+
+"
+    O operador ANY no PostgreSQL permite comparar um valor com um conjunto de 
+    valores dentro de um array ou o resultado de uma subconsulta. Ele retorna 
+    verdadeiro se o valor sendo comparado corresponder a qualquer elemento 
+    dentro do conjunto especificado e falso caso contrário.
+"

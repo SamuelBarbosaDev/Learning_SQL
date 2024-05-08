3bae3baf28c60d5eb3674594212e7cc48765a2c2
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-05-08 13:55:22 -0300

    feat: Estudo sobre CASE no PostgreSQL

---
+++ postgres/28_case.sql
@@ -0,0 +1,16 @@
+SELECT
+	product_name,
+	CASE
+		WHEN price < 10 THEN 'Low price product'
+		WHEN price > 50 THEN 'High price product'
+	ELSE
+		'Normal Product'
+	END AS 'Price Category'
+FROM products
+'
+    O operador CASE no PostgreSQL permite criar expressões condicionais 
+    que produzem resultados diferentes com base em condições especificadas. 
+    Ele é semelhante a uma instrução IF/ELSE em outras linguagens de programação 
+    e pode ser usado em qualquer lugar onde você usaria uma expressão, 
+    como nas cláusulas SELECT, WHERE, GROUP BY e HAVING
+'

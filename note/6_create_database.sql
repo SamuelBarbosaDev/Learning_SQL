885dea5c4d8c68223351aa2a68e8bbe2fb9fb602
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-07-12 17:35:34 -0300

    feat: Estudo sobre `CREATE DATABASE`

---
+++ note/6_create_database.sql
@@ -0,0 +1,8 @@
+-- Cria base de dados:
+CREATE DATABASE hello_wolrd_afonso;
+
+-- Criando base de dados especificando usuário:
+CREATE DATABASE afonsodb owner = afonso;
+
+-- Criando base de dados especificando usuário, ENCODING e TEMPLATE:
+CREATE DATABASE afonsodb owner = afonso ENCODING = 'UTF8' TEMPLATE template0;
